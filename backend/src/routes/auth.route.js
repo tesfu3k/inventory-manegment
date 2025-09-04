7ced24f45d870372800e260626c1f27603210069
tesfu3k <tesfaye4r@gmail.com>
2025-09-04 10:31:25 +0300

    auth controllers are included

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+const route = new Router();
+import {
+  currentUser,
+  signIn,
+  signOut,
+  signUp,
+} from "../controllers/auth.controllers.js";
+import { protectRoute } from "../../middlewares/auth.middleware.js";
+
+route.post("/sign-up", signUp);
+route.post("/sign-in", signIn);
+route.get("/sign-out", signOut);
+route.get("/me", protectRoute, currentUser);
+
+export default route;
