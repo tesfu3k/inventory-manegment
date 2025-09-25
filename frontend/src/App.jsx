d79c8d0e6088634134d74750f8b0a8231fd66995
tesfu3k <tesfaye4r@gmail.com>
2025-09-25 16:40:44 +0300

    feat: add LogIn pages with form functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+
+import LogIn from "./pages/LogIn";
 const App = () => {
-  return <div className="text-2xl text-red-800">App</div>;
+  return (
+    <Routes>
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/log-in" element={<LogIn />} />
+    </Routes>
+  );
 };
 
 export default App;
