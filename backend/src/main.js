333ba6a7931fa4613c7169083982b55f89a4aa9d
tesfu3k <tesfaye4r@gmail.com>
2025-09-25 22:15:13 +0300

    feat: add CORS support and enhance SignUp form with validation and submission logic

--- backend/src/main.js
+++ backend/src/main.js
@@ -4,6 +4,7 @@ import authRoute from "./routes/auth.routes.js";
 import employeesRoute from "./routes/employee.routes.js";
 import inventoryRoute from "./routes/inventory.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import { connectDB } from "../src/config/db.config.js";
 
@@ -12,6 +13,7 @@ const app = express();
 // Middleware
 app.use(express.json()); // will allow to read req.body
 app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 const port = process.env.PORT || 3000;
 
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,10 +1,49 @@
 import { Mail, LockKeyhole, User } from "lucide-react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import axios from "axios";
 const SignUp = () => {
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+  const handleOnChange = (e) => {
+    const { value, name } = e.target;
+
+    setUserData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!userData.name || !userData.email || !userData.password)
+      return toast.error("Enter all reqired data");
+
+    try {
+      const { data } = await axios.post(
+        "http://localhost:3000/api/auth/sign-up",
+        userData,
+        { validateStatus: (status) => status < 500, withCredentials: true }
+      );
+
+      if (data.success) {
+        navigate("/log-in");
+        return toast.success(data.message);
+      }
+      if (!data.success) return toast.error(data.message);
+    } catch (error) {
+      toast.error(
+        error.message || "Something went wrong. Please try again later"
+      );
+    }
+  };
   return (
     <div className="min-h-screen text-white flex items-center justify-center bg-[url('/bg.jpg')] bg-no-repeat bg-center bg-cover">
       <div className="relative w-full max-w-sm rounded-2xl bg-gradient-to-tl from-white/30 via-transparent to-white/30 backdrop-blur-xl border-[rgba(255,255,255,0.5)] border-2 p-8 shadow-xl">
-        <form noValidate>
+        <form onSubmit={handleOnSubmit}>
           <h1 className="mb-6 text-center text-2xl font-semibold">SingUp</h1>
           {/* Name */}
           <div className="relative mx-auto mb-6 w-full">
@@ -16,9 +55,11 @@ const SignUp = () => {
               id="name"
               name="name"
               type="text"
+              value={userData.name}
+              onChange={handleOnChange}
+              placeholder=""
               required
               autoComplete="name"
-              placeholder=""
               className="peer w-full rounded-md bg-transparent px-3 pr-10 pb-2 pt-5 text-base outline-none ring-1 ring-white/60 transition focus:ring-2 focus:ring-white/90"
             />
 
@@ -42,9 +83,11 @@ const SignUp = () => {
               id="email"
               name="email"
               type="email"
+              value={userData.email}
+              onChange={handleOnChange}
+              placeholder=""
               required
               autoComplete="email"
-              placeholder=" "
               className="peer w-full rounded-md bg-transparent px-3 pr-10 pb-2 pt-5 text-base outline-none ring-1 ring-white/60 transition focus:ring-2 focus:ring-white/90"
             />
 
@@ -69,9 +112,11 @@ const SignUp = () => {
               id="password"
               name="password"
               type="password"
+              value={userData.password}
+              onChange={handleOnChange}
+              placeholder=""
               required
               autoComplete="current-password"
-              placeholder=" "
               className="peer w-full rounded-md bg-transparent px-3 pr-10 pb-2 pt-5 text-base outline-none ring-1 ring-white/60 transition focus:ring-2 focus:ring-white/90"
             />
 
