ddd07f185757158c3c0cbb1001a1ac43046d8d21
tesfu3k <tesfaye4r@gmail.com>
2025-10-06 11:28:03 +0300

    feat(employee): enhance AddEmployee form with job information fields and salary input

--- frontend/src/pages/AddEmployee.jsx
+++ frontend/src/pages/AddEmployee.jsx
@@ -21,7 +21,7 @@ const AddEmployee = () => {
                 className="text-md text-cyan-500 font-medium"
                 htmlFor="firstName"
               >
-                First Name
+                First Name <span className="text-red-500">*</span>
               </label>
               <input
                 className="w-full mt-1 border border-cyan-300 rounded-md px-3 py-2 text-sm outline-0 focus:ring-4 focus:ring-cyan-500/20"
@@ -118,6 +118,106 @@ const AddEmployee = () => {
               placeholder="Enter Address"
             />
           </div>
+        </div>
+
+        <hr className="my-10 border-cyan-800/10 b" />
+
+        <h1 className="text-2xl font-bold mb-3 max-lg:text-lg max-lg:text-center">
+          Job Information
+        </h1>
+        <div className=" flex gap-3 lg:gap-20 max-lg:flex-col">
+          <div className="flex flex-col flex-1">
+            <label
+              className="text-md text-cyan-500 font-medium "
+              htmlFor="position"
+            >
+              Position
+            </label>
+            <select
+              name="position"
+              id="position"
+              className="w-full mt-1 border border-cyan-300 rounded-md px-3 py-2 text-sm outline-0 focus:ring-4 focus:ring-cyan-500/20"
+            >
+              <option disabled selected>
+                Select position
+              </option>
+              <option value="engineer">Engineer</option>
+              <option value="seniorEngineer">Senior Engineer</option>
+              <option value="manager">Manager</option>
+              <option value="director">Director</option>
+              <option value="intern">Intern</option>
+              <option value="designer">Designer</option>
+            </select>
+          </div>
+
+          <div className="flex flex-col flex-1">
+            <label
+              className="text-md text-cyan-500 font-medium "
+              htmlFor="department"
+            >
+              Department
+            </label>
+            <select
+              name="department"
+              id="department"
+              className="w-full mt-1 border border-cyan-300 rounded-md px-3 py-2 text-sm outline-0 focus:ring-4 focus:ring-cyan-500/20"
+            >
+              <option disabled selected>
+                Select department
+              </option>
+              <option value="engineering">Engineering</option>
+              <option value="design">Design</option>
+              <option value="sales">Sales</option>
+              <option value="hr">HR</option>
+              <option value="finance">Finance</option>
+              <option value="operations">Operations</option>
+            </select>
+          </div>
+
+          <div className="flex flex-col flex-1  relative">
+            <label
+              className="text-md text-cyan-500 font-medium"
+              htmlFor="salary"
+            >
+              Salary
+            </label>
+            <input
+              className="w-full mt-1 border border-cyan-300 rounded-md px-3 py-2 text-sm outline-0 focus:ring-4 focus:ring-cyan-500/20"
+              id="salary"
+              type="text"
+              placeholder="Enter salary"
+            />
+            <span className="absolute top-8 right-4">ETB</span>
+            <p className="text-xs hidden lg:block">
+              Enter amount in Ethiopian Birr
+            </p>
+          </div>
+
+          <div className="flex flex-col flex-1">
+            <label
+              className="text-md text-cyan-500 font-medium "
+              htmlFor="startDate"
+            >
+              Start Date
+            </label>
+            <input
+              className="w-full mt-1 border border-cyan-300 rounded-md px-3 py-2 text-sm outline-0 focus:ring-4 focus:ring-cyan-500/20"
+              id="startDate"
+              type="date"
+              placeholder="Enter start date"
+            />
+          </div>
+        </div>
+
+        <hr className="my-10 border-cyan-800/10" />
+
+        <div className="flex gap-8 my-10 text-lg font-medium lg:gap-40 ">
+          <button className="w-full border border-cyan-800 rounded-xl py-3 hover:bg-cyan-100 active:scale-95 cursor-pointer">
+            Cancel
+          </button>
+          <button className="w-full bg-cyan-800 rounded-xl text-white py-3 hover:bg-cyan-600 active:scale-95 cursor-pointer">
+            Save Employee
+          </button>
         </div>
       </div>
     </form>
